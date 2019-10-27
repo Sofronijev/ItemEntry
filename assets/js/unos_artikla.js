5b500079dde160c673bb45e8b878b42e5f6ae073
Sofronijev <sofronijev@gmail.com>
2019-10-27 16:10:21 +0100

    added filtering

--- assets/js/unos_artikla.js
+++ assets/js/unos_artikla.js
@@ -13,15 +13,92 @@ $(document).ready(() => {
     let slika = $("#slika");
     let errSlika = $("#errSlika");
     let tabela = $('#tabela tbody');
+    let pretraga = $('#pretraga');
+    let obrisiBtn = $('#obrisi');
 
 
     function checkLocalStorage() {
         return localStorage.getItem('artikli') ? JSON.parse(localStorage.getItem('artikli')) : [];
     }
 
+    //Prikaz slike
+    function prikazSlike(input) {
+        if (input.files && input.files[0]) {
 
-    //Proverava da li postoji nesto u localstorige, ako ima cuva podatke ako je prazno pravi prazan niz
-    let artikli = checkLocalStorage();
+            //uzimamo naziv slike
+            let nazivSlike = input.files[0].name;
+            //delimo string po "."
+            let split = nazivSlike.split(".");
+            //uzimamo poslednji deo jer se tu nalazi ekstenzija
+            let ext = split[split.length - 1];
+
+            if (ext.toLowerCase() === "png") {
+                errSlika.text(nazivSlike);
+                errSlika.removeClass("text-danger bad-input");
+                slika.removeClass("bad-input");
+
+                //FileReader objekat asinhrono cita sadrzaj fajla skoji se nalazi na korisnickom kompjuteru
+                let reader = new FileReader();
+                //kad je uspesno zavrseno citanje upisuje u src
+                reader.onload = function (e) {
+                    $('#prikaz-slike').attr('src', e.target.result);
+                }
+                //cita sadrzaj fajla
+                reader.readAsDataURL(input.files[0]);
+            } else {
+                errSlika.text("Slika mora da bude u png formatu");
+                errSlika.addClass("text-danger bad-input");
+                slika.addClass("bad-input");
+            }
+        }
+    }
+
+    //funkcija za dodavanje artikla u localstorage
+    function addToLocalStorage() {
+
+        let artikli = checkLocalStorage();
+        //kreiramo novi artikl
+        let artikl = new Artikl(barkod.val(), naziv.val(), opis.val(), vrsta.val(), osCena.val(), pdvCena.val(), slika.val());
+
+        //ako je ubacena neka slika menjamo joj vrednost u Base 64
+        if (slika.val()) {
+            let reader = new FileReader();
+            reader.onload = function (e) {
+                artikl.slika = e.target.result;
+            }
+            reader.readAsDataURL(slika[0].files[0]);
+        }
+        //settimeout je da bi se zavrsila funkcija onload
+        setTimeout(() => {
+            artikli.push(artikl);
+            localStorage.setItem('artikli', JSON.stringify(artikli));
+        }, 500);
+    }
+
+    //funkcija za ispis artikla
+    function ispisArtikla(broj) {
+        setTimeout(() => {
+            let artikli = checkLocalStorage();
+            jQuery.each(artikli, function (index, artikl) {
+                //proverava da li se ispisuju svi artikli ili samo jedan novi
+                if (broj === 1 ? index == artikli.length - 1 : true) {
+                    let datum = new Date(artikl.datum);
+                    let cena = artikl.cena ? parseInt(artikl.osCena).toFixed(2) : "";
+                    tabela.append(` <tr>
+                            <td>${artikl.barkod}</td>
+                            <td>${artikl.naziv}</td>
+                            <td>${artikl.opis}</td>
+                            <td>${artikl.vrsta}</td>
+                            <td>${cena}</td>
+                            <td>${artikl.pdvCena}</td>
+                            <td><img class="td-slika" src="${artikl.slika}"></td>
+                            <td>${datum.toLocaleString()}</td>
+                            </tr>`
+                    );
+                }
+            })
+        }, 600);
+    }
     //kreiranje klase za pravljenje objekata artikl
     class Artikl {
 
@@ -36,6 +113,9 @@ $(document).ready(() => {
                 this.datum = new Date()
         }
     }
+
+    //Proverava da li postoji nesto u localstorige, ako ima cuva podatke ako je prazno pravi prazan niz
+    let artikli = checkLocalStorage();
 
     //Validacija podataka
 
@@ -93,7 +173,6 @@ $(document).ready(() => {
             //proverava da li je prazna vrednost cene, ako jeste brise cenu sa pdv
             if (osCena.val() !== "") {
                 let cenaSaPdv = (parseInt(osCena.val()) * 1.2).toFixed(2);
-                console.log(typeof (cenaSaPdv));
                 pdvCena.val(cenaSaPdv);
             } else pdvCena.val("");
         } else {
@@ -102,89 +181,15 @@ $(document).ready(() => {
         }
     });
 
-    //Prikaz slike
-    function prikazSlike(input) {
-        if (input.files && input.files[0]) {
 
-            //uzimamo naziv slike
-            let nazivSlike = input.files[0].name;
-            //delimo string po "."
-            let split = nazivSlike.split(".");
-            //uzimamo poslednji deo jer se tu nalazi ekstenzija
-            let ext = split[split.length - 1];
-
-            if (ext.toLowerCase() === "png") {
-                errSlika.text(nazivSlike);
-                errSlika.removeClass("text-danger bad-input");
-                slika.removeClass("bad-input");
-
-                //FileReader objekat asinhrono cita sadrzaj fajla skoji se nalazi na korisnickom kompjuteru
-                let reader = new FileReader();
-                //kad je uspesno zavrseno citanje upisuje u src
-                reader.onload = function (e) {
-                    $('#prikaz-slike').attr('src', e.target.result);
-                }
-                //cita sadrzaj fajla
-                reader.readAsDataURL(input.files[0]);
-            } else {
-                errSlika.text("Slika mora da bude u png formatu");
-                errSlika.addClass("text-danger bad-input");
-                slika.addClass("bad-input");
-            }
-        }
-    }
     //prikaz slike
     $("#slika").change(function () {
         prikazSlike(this);
     });
-    function addToLocalStorage() {
-
-        let artikli = checkLocalStorage();
-        //kreiramo novi artikl
-        let artikl = new Artikl(barkod.val(), naziv.val(), opis.val(), vrsta.val(), osCena.val(), pdvCena.val(), slika.val());
-
-        //ako je ubacena neka slika menjamo joj vrednost u Base 64
-        if (slika.val()) {
-            let reader = new FileReader();
-            reader.onload = function (e) {
-                artikl.slika = e.target.result;
-            }
-            reader.readAsDataURL(slika[0].files[0]);
-        }
-        //settimeout je da bi se zavrsila funkcija onload
-        setTimeout(() => {
-            artikli.push(artikl);
-            localStorage.setItem('artikli', JSON.stringify(artikli));
-        }, 500);
-    }
-
-    function ispisArtikla(broj) {
-        setTimeout(() => {
-            let artikli = checkLocalStorage();
-            console.log(artikli);
-            jQuery.each(artikli, function (index, artikl) {
-                //proverava da li se ispisuju svi artikli ili samo jedan novi
-                if (broj === 1 ? index == artikli.length - 1 : true) {
-                    let datum = new Date(artikl.datum);
-                    tabela.append(` <tr>
-                            <td>${artikl.barkod}</td>
-                            <td>${artikl.naziv}</td>
-                            <td>${artikl.opis}</td>
-                            <td>${artikl.vrsta}</td>
-                            <td>${artikl.osCena}</td>
-                            <td>${artikl.pdvCena}</td>
-                            <td><img class="td-slika" src="${artikl.slika}"></td>
-                            <td>${datum.toLocaleString()}</td>
-                            </tr>`
-                    );
-
-                }
-
-            })
-        }, 600);
-    }
+    //ispisuje kad se ucitava stranica
     ispisArtikla();
 
+    // kad se pritisne dugme za unos artikla
     $("#unos_artikla").submit(function (e) {
         e.preventDefault();
         //i ako su polje required, ovo je dodatna provera
@@ -193,9 +198,45 @@ $(document).ready(() => {
             return;
         }
         addToLocalStorage();
+
         //ispisuje samo 1 artikl, onaj koji se sad napravio
         ispisArtikla(1);
 
     });
 
+    //filtriranje po nazivu
+    pretraga.keyup(() => {
+
+        let nazivPretraga = pretraga.val();
+        let artikli = checkLocalStorage();
+
+        let filter = artikli.filter((artikl) => {
+            if ((artikl.naziv).toLowerCase().includes(nazivPretraga.toLowerCase())) {
+                return artikl;
+            }
+
+        });
+        //prazni tabelu da se ne bi redovi duplirali
+        tabela.empty();
+        jQuery.each(filter, function (index, artikl) {
+            let datum = new Date(artikl.datum);
+            let cena = artikl.cena ? parseInt(artikl.osCena).toFixed(2) : "";
+            tabela.append(` <tr>
+                            <td>${artikl.barkod}</td>
+                            <td>${artikl.naziv}</td>
+                            <td>${artikl.opis}</td>
+                            <td>${artikl.vrsta}</td>
+                            <td>${cena}</td>
+                            <td>${artikl.pdvCena}</td>
+                            <td><img class="td-slika" src="${artikl.slika}"></td>
+                            <td>${datum.toLocaleString()}</td>
+                            </tr>`
+            );
+        })
+    });
+
+    obrisiBtn.click(function () {
+        localStorage.removeItem('artikli');
+        tabela.empty();
+    });
 });
